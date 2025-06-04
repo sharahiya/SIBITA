f74aa57b536d57fcbbfce904132afca2cddd6542
Papazy <fajryjobs@gmail.com>
2025-06-04 18:33:28 +0700

    refactor: Perbaiki UploadBerkasController

--- app/Http/Controllers/UploadBerkasController.php
+++ app/Http/Controllers/UploadBerkasController.php
@@ -75,6 +75,7 @@ public function upload(Request $request)
 
     if ($jenis === 'sidang') {
         // commit baru
+        // commir baru lagi
         $semhas = Seminar::where('id_mahasiswa', $mahasiswa->id_mahasiswa)
             ->where('jenis', 'hasil')
             ->where('status', 'diterima')
